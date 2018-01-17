525a82ba17998ba2c8c5b81e3237b1b7c13bd3a1
sauravMir <saurav_41@yahoo.com>
2018-01-17 18:34:47 +0600

    work done

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express=require("express");
+var bodyParser = require('body-parser');
 var app=express();
 var exphbs     = require('express-handlebars');
 
+var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -11,14 +13,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
  //For Handlebars
- app.set('views', './app/views');
+ app.set('views', './public/views');
  app.engine('hbs', exphbs({extname: '.hbs'}));
  app.set('view engine', '.hbs');
 
 
  //Routes
- var authRoute = require('./app/routes/auth.js')(app);
+ var authRoute = require('./routes/auth.js')(app);
 
 app.get("/", function(req, res){
-res.send()
+res.render('index');
 });
+
+// the port and the Ip address
+app.listen("1025", "0.0.0.0", function(){
+    console.log("server done");
+  } );
